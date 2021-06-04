09b8a0dfb025daeaaee8eadb8a012f28ccc87173
xuyang1 <xuyang1@huya.com>
2021-06-04 07:57:43 +0000

    ok

--- src/ques1-1.ts
+++ src/ques1-1.ts
@@ -1,6 +1,6 @@
 import { Axis } from './axis';
 import { World } from './world';
-import { cross, dot, unit } from './matrix';
+import { cross, dot, normal } from './matrix';
 
 const world = new World();
 const axis = new Axis(world, {
@@ -11,12 +11,8 @@ axis.render();
 
 // 问题：判断v1和v2分别在target的上还是下
 const target = [4, 4];
-const v1 = [2, 4];
+const v1 = [1, 2];
 const v2 = [2, 1];
-
-const targetUnit = unit(target);
-const v1Unit = unit(v1);
-const v2Unit = unit(v2);
 
 axis.renderV(target);
 axis.renderV(v1, 'yellow');
@@ -26,18 +22,19 @@ axis.renderV(v2, 'blue');
 const a = cross(target, v1);
 const b = cross(target, v2);
 
-console.log(a, b);
+console.log(a, b, 'cross');
 // a>0 在target的左边（上）， b<0在target的右边（下）
 
-// 解法2：
-const n = [0, 1];
-const d = dot(targetUnit, n);
+// 解法2： 用点积算叉积
+const n = normal(target);
 
-// cos函数是交越大值约小，所以v1d如果是正数说明它的角比target的角小，它就在target的上边
-// 反之在下边
+// const d = dot(target, n);
 
-const v1d = dot(v1Unit, n) - d;
-const v2d = dot(v2Unit, n) - d;
+// sin x = cos(x - 90)
+// sin x = cos(x + 90) * -1
+const v1d = dot(v1, n) * -1;
+const v2d = dot(v2, n) * -1;
 
-console.log(v1d, 'v1');
-console.log(v2d, 'v2');
+// v1d0 在target的左边（上）， v2d<0在target的右边（下）
+
+console.log(v1d, v2d, 'dot');
